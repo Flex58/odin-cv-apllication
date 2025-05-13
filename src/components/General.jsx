932fa353d3848cb043444291e6e552be115ef7f0
Flex58 <164533598+flex58@users.noreply.github.com>
2025-05-13 09:41:31 +0200

    build: add mdiIcons

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,10 +1,19 @@
-export default function General({firstName, lastName, telephone, email}) {
-    const fullName = firstName + " " + lastName
-    return (
-        <>
-        <h2>{fullName}</h2>
+import Icon from "@mdi/react";
+import { mdiEmail, mdiPhone } from "@mdi/js";
+
+export default function General({ firstName, lastName, telephone, email }) {
+  const fullName = firstName + " " + lastName;
+  return (
+    <div className="generalContainer">
+      <h2>{fullName}</h2>
+      <div className="infoContainer">
+        <Icon path={mdiPhone} size={1} />
         <p>{telephone}</p>
-        <p>{email}</p>
-        </>
-    )
+      </div>
+      <div className="infoContainer">
+      <Icon path={mdiEmail} size={1}/>  
+      <p>{email}</p>
+      </div>
+    </div>
+  );
 }
